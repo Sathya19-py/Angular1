f175db6ff60c4fc98c87fc05b857b7b9cb08365c
Sathya19-py <57808746+Sathya19-py@users.noreply.github.com>
2020-11-21 22:01:38 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Identifiers } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Security';
+  // title = 'Security';
+  
+  public home_status:boolean = true;
+  public agent_status:boolean = true;
+  public option:string;
+  
+  
+  public open(option)
+  {
+    if(option == 'home'){
+      console.log(option);
+    };
+
+    if(option == 'agent'){
+      console.log(option);
+    };
+
+    if(option == 'defence'){
+      console.log(option);
+    };
+
+    if(option == 'admin'){
+      console.log(option);
+    };
+    
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { DefenceComponent } from './defence/defence.component';
+import { AgentComponent } from './agent/agent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    DefenceComponent,
+    AgentComponent
   ],
   imports: [
     BrowserModule,
